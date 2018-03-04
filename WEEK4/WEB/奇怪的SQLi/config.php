44da1ac44e6a2bcfb1bddc5a6cd92b5258ba35b5
alias <yiyezhiqiu233@gmail.com>
2018-03-04 20:45:08 +0800

    Update config.php

--- WEEK4/WEB/奇怪的SQLi/config.php
+++ WEEK4/WEB/奇怪的SQLi/config.php
@@ -1,9 +1,9 @@
 <?php
 error_reporting(0);
-
-$db_host = 'localhost';
+// 题目是线上改的 下面是凭印象复原的
+$db_host = 'mysql';
 $db_name = 'users';
-$db_user = 'root';
+$db_user = 'week4';
 $db_pwd = '';
 
 $conn = mysqli_connect($db_host, $db_user, $db_pwd, $db_name);
